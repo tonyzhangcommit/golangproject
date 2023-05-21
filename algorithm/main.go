7c467c022674f2d961a3c1589b42511e93677118
tony <775273490@qq.com>
2023-05-21 15:19:28 +0800

    hello world

--- algorithm/main.go
+++ algorithm/main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+type Person struct {
+	age int
+}
+
 func main() {
 	fmt.Println("hello world!")
+
+	p := &Person{29}
+
+	defer fmt.Println(p.age)
+	
+	defer func(p *Person) {
+		fmt.Println(p.age)
+	}(p)
+
+	defer func() {
+		fmt.Println(p.age)
+	}()
+
+	p = &Person{30}
+
 }
